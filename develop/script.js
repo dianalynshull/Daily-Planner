b1ee56dc19f9ea87f21843d0c6295c78581e204d
dianalynshull <dianalynshull@gmail.com>
2020-08-22 22:18:55 -0600

    got the save button image to work. Also integrated moment js to adjust the appearance of the background color based on the time

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,5 @@
 // when text is typed into the time spot it is saved in local storage
-const timeSlots = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+const timeSlots = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 let plannerTimeSlot;
 let bsInputGroup;
@@ -9,38 +9,44 @@ let bsTextArea;
 let bsInputGroupAppend;
 let bsSubmitButton;
 let amPm = "";
-let faSaveImg = 'i class="far fa-save"';
+let plannerLocalStorage;
+let currentDay = moment().format("dddd MMM Do YYYY");
+let currentHour = moment().format("H")
+console.log(currentHour);
 
 plannerInput();
 
 function plannerInput() {
     for (let i = 0; i < timeSlots.length; i++) {
+        if (currentHour > timeSlots[i]) {
+            bsTextArea.addClass("past");
+        };
+        let timeSlotStandard = moment(timeSlots[i], "H").format("h");
+        console.log(timeSlotStandard);
         // Variables to create planner elements
         plannerTimeSlot = $("<form>");
         bsInputGroup = $("<div>").addClass("input-group flex-nowrap");
         bsInputGroupPrepend = $("<div>").addClass("input-group-prepend");
         bsInputGroupText = $("<span>").addClass("input-group-text addon-wrapping");
-        bsTextArea = $("<text-area>").addClass("form-control").attr("id",  "text-area-" + timeSlots[i]);
+        bsTextArea = $("<textarea>").addClass("form-control").attr("id",  "text-area-" + timeSlotStandard);
         bsInputGroupAppend = $("<div>").addClass("input-group-append");
-        bsSubmitButton = $("<button>").addClass("btn btn-outline-secondary button-addon2").attr("type", "button").attr("id", "save-btn-" + timeSlots[i]);
+        bsSubmitButton = $("<button>").addClass("btn btn-outline-secondary button-addon2").attr("type", "button").attr("id", "save-btn-" + timeSlotStandard);
         
         // defines if the timeslot is am or pm
         if (timeSlots[i] >= 9 && timeSlots[i] < 12) {
             amPm = " AM";
-            bsInputGroupText.textContent = timeSlots[i] + amPm;
+            bsInputGroupText.text(timeSlotStandard + amPm);
         } else {
             amPm = " PM";
-            bsInputGroupText.textContent = timeSlots[i] + amPm;
+            bsInputGroupText.text(timeSlotStandard + amPm);
         }
+
+        //defines color of planner element background
         
         // adding text content to planner
-        bsInputGroupText.text(timeSlots[i] + amPm);
-        bsTextArea.text("test");
-        bsSubmitButton.text("<" + faSaveImg + ">");
+        bsSubmitButton.html("<i class='far fa-save'>");
 
-        
-
-
+        $("#currentDay").text(currentDay);
         $("#container").append(plannerTimeSlot);
         plannerTimeSlot.append(bsInputGroup);
         bsInputGroup.append(bsInputGroupPrepend);
@@ -49,15 +55,12 @@ function plannerInput() {
 
         bsInputGroupPrepend.append(bsInputGroupText);
         bsInputGroupAppend.append(bsSubmitButton);
-
-        console.dir(bsInputGroupText.textContent);
-        console.dir(bsTextArea.textContent);
-        console.dir(bsSubmitButton.textContent);
-
+        
         //style planner
         $(".addon-wrapping").width("50px");
-
+        
     }
 }
+
 
     
